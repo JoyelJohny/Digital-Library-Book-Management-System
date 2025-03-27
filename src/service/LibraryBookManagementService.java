34d6580227fff409006a205ffb15f68daaade60a
JoyelJohny <joyel2johny@gmail.com>
2025-03-27 15:31:12 +0530

    Added LibraryBookManagementService fields and methods

--- src/service/LibraryBookManagementService.java
+++ src/service/LibraryBookManagementService.java
@@ -1,8 +1,24 @@
 package service;
 
+import model.Book;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LibraryBookManagementService {
 
-    public LibraryBookManagementService(){
+    // Used Hashmap for fast searching of books.
+    private final Map<String, Book> books;
 
+    public LibraryBookManagementService(){
+        this.books = new HashMap<>();
     }
+
+    private void addBook(){}
+    private void updateBook(){}
+    private void deleteBook(){}
+    private void listAllBooks(){}
+    private void searchBook(){}
+
+
 }
