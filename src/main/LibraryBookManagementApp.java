1db054e69d17d33248a465cc0a16f1c71a56d9df
JoyelJohny <joyel2johny@gmail.com>
2025-03-27 14:42:33 +0530

    Added file structure

---
+++ src/main/LibraryBookManagementApp.java
@@ -0,0 +1,10 @@
+package main;
+
+import ui.LibraryBookManagementUI;
+
+public class LibraryBookManagementApp {
+
+    public static void main(String[] args) {
+        LibraryBookManagementUI ui = new LibraryBookManagementUI();
+    }
+}
